72b5fe6525254e52c254e464db745db577f9e78e
nisihara1 <nisihara.burai@gmail.com>
2017-02-28 15:27:00 +0900

    punch 1D-RISM as XML.

--- Modules/xml_io_rism.f90
+++ Modules/xml_io_rism.f90
@@ -26,7 +26,11 @@ MODULE xml_io_rism
   PRIVATE
   !
   ! ... define constants
+#ifdef __1DRISM_BINARY
   LOGICAL :: l1drism_binary = .TRUE.
+#else
+  LOGICAL :: l1drism_binary = .FALSE.
+#endif
   LOGICAL :: l3drism_binary = .TRUE.
   !
   ! ... public components
