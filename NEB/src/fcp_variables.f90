4e3c4d35924dcfcf1c2dc8a4c71c6bdb5e94861f
nisihara1 <nisihara.burai@gmail.com>
2017-06-30 10:27:15 +0900

    modify fcp_variables

--- NEB/src/fcp_variables.f90
+++ NEB/src/fcp_variables.f90
@@ -1,30 +1,40 @@
 !
-! Copyright (C) 2002-2011 Quantum ESPRESSO group
+! Copyright (C) 2002-2017 Quantum ESPRESSO group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
-!------------------------------------------------------------------------------!
+!--------------------------------------------------------------------------
 MODULE fcp_variables
-!------------------------------------------------------------------------------!
-   !
-   USE kinds,      ONLY : DP
-   !
-   IMPLICIT NONE
-   SAVE
-   !
-   LOGICAL          :: lfcpopt              = .FALSE.
-   LOGICAL          :: lfcpdyn              = .FALSE.
-   REAL(DP)         :: fcp_mu               = 0.0_DP
-   REAL(DP)         :: fcp_mass             = 10000.0_DP
-   REAL(DP)         :: fcp_temperature      = 0.0_DP
-   CHARACTER(LEN=8) :: fcp_relax            = 'mdiis'
-   REAL(DP)         :: fcp_relax_step       = 0.5_DP
-   REAL(DP)         :: fcp_relax_crit       = 0.001_DP
-   INTEGER          :: fcp_mdiis_size       = 4
-   REAL(DP)         :: fcp_mdiis_step       = 0.2_DP
-   REAL(DP)         :: fcp_tot_charge_first = 0.0_DP
-   REAL(DP)         :: fcp_tot_charge_last  = 0.0_DP
-
+  !--------------------------------------------------------------------------
+  !
+  USE kinds,      ONLY : DP
+  !
+  IMPLICIT NONE
+  !
+  SAVE
+  !
+  LOGICAL :: &
+       lfcp             = .FALSE.   ! if .TRUE. FCP is optimized too.
+  !
+  REAL(DP) :: &
+       fcp_mu           = 0.0_DP    ! target Fermi energy,
+                                    ! in Rydberg
+  !
+  LOGICAL :: &
+       lfcp_linmin      = .FALSE., &! .TRUE. if fcp_scheme = "lm"
+       lfcp_newton      = .FALSE.   ! .TRUE. if fcp_scheme = "newton"
+  !
+  REAL(DP) :: &
+       fcp_thr          = 1.0E-3_DP ! convergence threshold for FCP relaxation,
+                                    ! in Rydberg
+  !
+  INTEGER :: &
+       fcp_ndiis        = 4         ! size of DIIS for Newton-Raphson algorithm
+  !
+  REAL(DP) :: &
+       tot_charge_first = 0.0_DP,  &! total charge of the first image
+       tot_charge_last  = 0.0_DP    ! total charge of the last image
+  !
 END MODULE fcp_variables
