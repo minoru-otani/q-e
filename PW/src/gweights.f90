a166603440da724e80f04a67dfa663165521ffbe
nisihara1 <nisihara.burai@gmail.com>
2017-07-25 18:01:15 +0900

    tune gweights.f90

--- PW/src/gweights.f90
+++ PW/src/gweights.f90
@@ -33,7 +33,7 @@ subroutine gweights (nks, wk, nbnd, nelec, degauss, ngauss, &
   ef_now = efermig (et, nbnd, nks, nelec, wk, degauss, ngauss, is, isk)
 
   if (beta > 0.0_DP) then
-     if (abs(ef - ef_now) > 0.01_DP) then
+     if (abs(ef - ef_now) > 0.05_DP) then
         ef = beta * ef + (1.0_DP - beta) * ef_now
      end if
   else
