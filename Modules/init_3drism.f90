ebc976129f077743d3989a082b9370a6fee171dd
nisihara1 <nisihara.burai@gmail.com>
2017-09-05 12:14:39 +0900

    fix init_3drism.f90

--- Modules/init_3drism.f90
+++ Modules/init_3drism.f90
@@ -43,47 +43,60 @@ SUBROUTINE init_3drism(rism3t, solu, lboth, ierr)
     IF (ierr /= IERR_RISM_NULL) THEN
       RETURN
     END IF
-    !
   END IF
   !
   ! ... create solvent's susceptibilities
   IF (rism3t%itype == ITYPE_3DRISM) THEN
     CALL suscept_vv(rism1t, rism3t, ierr)
     !
+    IF (ierr /= IERR_RISM_NULL) THEN
+      RETURN
+    END IF
+    !
   ELSE !IF (rism3t%itype == ITYPE_LAUERISM) THEN
     CALL rism1d_activate_right()
     CALL suscept_laue(rism1t, rism3t, bond_width, .TRUE., ierr)
+    !
+    IF (ierr /= IERR_RISM_NULL) THEN
+      RETURN
+    END IF
+    !
     IF (lboth) THEN
       CALL rism1d_activate_left()
       CALL suscept_laue(rism1t, rism3t, bond_width, .FALSE., ierr)
+      !
+      IF (ierr /= IERR_RISM_NULL) THEN
+        RETURN
+      END IF
     END IF
-  END IF
-  !
-  IF (ierr /= IERR_RISM_NULL) THEN
-    RETURN
   END IF
   !
   ! ... integrate solvent's susceptibilities
   IF (rism3t%itype == ITYPE_LAUERISM) THEN
     CALL rism1d_activate_right()
     CALL suscept_laueint(rism3t, .TRUE., ierr)
+    !
+    IF (ierr /= IERR_RISM_NULL) THEN
+      RETURN
+    END IF
+    !
     IF (lboth) THEN
       CALL rism1d_activate_left()
       CALL suscept_laueint(rism3t, .FALSE., ierr)
+      !
+      IF (ierr /= IERR_RISM_NULL) THEN
+        RETURN
+      END IF
     END IF
-  END IF
-  !
-  IF (ierr /= IERR_RISM_NULL) THEN
-    RETURN
   END IF
   !
   ! ... prepare dipole part of Laue-RISM equation
   IF (rism3t%itype == ITYPE_LAUERISM .AND. (.NOT. lboth)) THEN
     CALL eqn_lauedipole(rismt, .TRUE., .TRUE., ierr)
-  END IF
-  !
-  IF (ierr /= IERR_RISM_NULL) THEN
-    RETURN
+    !
+    IF (ierr /= IERR_RISM_NULL) THEN
+      RETURN
+    END IF
   END IF
   !
   ! ... normally done
